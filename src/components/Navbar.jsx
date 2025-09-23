644144179b2c4aac46b9559fd9b723150214c6d7
GurpreetCoding <142757310+GurpreetCoding@users.noreply.github.com>
2025-09-23 14:46:16 -0400

    added mobile responsive design to navbar and hero section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,33 @@
-import React from 'react'
+import React, {useState} from 'react'
 import logo from '../assets/logo-1.svg'
+import hamburger from '../assets/hamburger.svg'
 import '../styles/Navbar.css'
 import Button from '../components/Button.jsx'
 
 function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+  const [hasMargin, sethasMargin] = useState(false);
+
+  const toggleMenu = () => {
+    console.log("hi");
+    setIsOpen(!isOpen);
+    sethasMargin(!hasMargin);
+  }
+
   return (
-    <div className='navbar'>
+    <div className={`navbar ${hasMargin ? 'margin' : ''}`}>
       <div className='logo-container'>
         <img src={logo}></img>
         <div className='logo-text'>ROASTED</div>
       </div>
-      <div className='nav-links'>
+
+      <div className='hamburger-icon' onClick={toggleMenu}>
+        <img src={hamburger}></img>
+      </div>
+
+
+
+      <div className={`nav-links ${isOpen ? 'open' : ''}`}>
         <a href='#'><div className='nav-link'>Home</div></a>
         <a href='../App.jsx#menu'><div className='nav-link'>Menu</div></a>
         <a href='../App.jsx#about'><div className='nav-link'>About</div></a>
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,21 +1,34 @@
-import React from 'react'
+import React, { useState, useEffect} from 'react'
 import '../styles/Hero.css'
 import Button from '../components/Button'
 import img1 from '../assets/img-2.svg'
+import img2 from '../assets/img-2-resize.svg'
 
 function Hero() {
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 600);
+
+  useEffect(() => {
+    function handleResize() {
+      setIsMobile(window.innerWidth < 600);
+    }
+
+    window.addEventListener('resize', handleResize);
+
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
   return (
     <div className='hero-section'>
       <div className='hero-left'>
         <div className='hero-title'>Start your Morning with Something Better.</div>
         <div className='hero-tagline'>Coffee. Comfort. Community.</div>
         <div className='hero-buttons'>
-          <a href='../App.jsx#order/contact'><Button className="hero-left-button" name="Order" size="large" color="blue"/></a>
-          <a href='../App.jsx#menu'><Button name="Menu" size="large2" color="transparent"/></a>
+          <a href='../App.jsx#order/contact'><Button className="hero-left-button" name="Order" size={`${isMobile ? 'small' : 'large'}`} color="blue"/></a>
+          <a href='../App.jsx#menu'><Button name="Menu" size={`${isMobile ? 'small2' : 'large2'}`} color="transparent"/></a>
         </div>
       </div>
       <div className='hero-right'>
-        <img src={img1}></img>
+        <img className='hero-image' src={isMobile ? img2 : img1}></img>
       </div>
     </div>
   )
